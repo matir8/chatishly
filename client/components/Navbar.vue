7e9986dbd5584172fb5c48a0d3fd63760455e341
matir8 <martin.markov28@gmail.com>
2019-01-03 00:42:13 +0200

    Created navigation components

---
+++ client/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="navbar">
+    <v-toolbar
+      fixed
+      height="44"
+      app>
+      <v-toolbar-title>Chatishly</v-toolbar-title>
+      <v-spacer />
+      <div v-if="!$auth.loggedIn">
+        <v-btn
+          flat
+          nuxt
+          to="/auth/login"
+        >Login</v-btn>
+        <v-btn
+          flat
+          nuxt
+          to="/auth/register"
+        >Register</v-btn>
+      </div>
+      <div v-else>
+        <v-btn
+          flat
+          @click="logout"
+        >Logout</v-btn>
+      </div>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+import AuthService from '@/services/authService.js'
+
+export default {
+  methods: {
+    logout() {
+      this.$axios.delete('/auth/sign_out').then(() => {
+        AuthService.clearCredentials()
+        this.$router.go()
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ client/components/NavigationDrawer.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="navigation-drawer">
+    <v-navigation-drawer
+      mini-variant
+      mini-variant-width="50"
+      permanent
+      dark
+      app
+    >
+      <v-list dense>
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+        >
+          <v-list-tile-action>
+            <nuxt-link
+              :to="item.href"
+              class="nav-link"
+            >
+              <v-icon>{{ item.icon }}</v-icon>
+            </nuxt-link>
+          </v-list-tile-action>
+        </v-list-tile> 
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { href: '/', icon: 'home' },
+        // { href: '/profile', icon: 'account_box' },
+        { href: '/', icon: 'gavel' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+a.nav-link {
+  text-decoration: none;
+}
+
+a.nav-link i.v-icon:hover {
+  color: #ffa000;
+}
+
+.v-list__tile {
+  padding: 0px;
+}
+</style>
